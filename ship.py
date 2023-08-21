14251f54fa1d55a03edadd2ad4afc83d8c334114
Artur Sultanov <sultanov.personal@gmail.com>
2023-08-21 17:07:49 +0200

    Update

--- ship.py
+++ ship.py
@@ -9,5 +9,13 @@ class Ship():
         # Loading starship image
         self.image = pygame.image.load('images/ship-0.bmp')
         self.rect = self.image.get_rect()
+        self.screen_rect = screen.get_rect()
 
+        # Each new ship appears at the bottom of the screen.
+        self.rect.centerx = self.screen_rect.centerx
+        self.rect.bottom = self.screen_rect.bottom
+
+    def blitme(self):
+        """Draws the ship at the current position."""
+        self.screen.blit(self.image, self.rect)
 
