ce8a8ead34c23b103927a068ab7e527b106dc586
Artur Sultanov <sultanov.personal@gmail.com>
2023-08-21 11:17:53 +0200

    Created window of the game and setting file

---
+++ alien_invasion.py
@@ -0,0 +1,23 @@
+import sys
+import pygame
+from settings import Settings
+
+def run_game():
+    # Initializes  pygame, settings and creates a screen object.
+    pygame.init()
+    ai_settings = Settings()
+    screen = pygame.display.set_mode(
+        (ai_settings.screen_width, ai_settings.screen_height))
+    pygame.display.set_caption("Alien Invasion")
+
+    # Starting the main game cycle.
+    while True:
+        # Tracking keyboard and mouse events.
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+        screen.fill(ai_settings.bg_color)
+        pygame.display.flip()
+
+
+run_game()
---
+++ settings.py
@@ -0,0 +1,10 @@
+class Settings():
+    """Class for saving settings of the game."""
+
+    def __init__(self):
+        """Initializes game settings. """
+        # Screen parameters
+        self.screen_width = 1024
+        self.screen_height = 640
+        self.bg_color = (40, 10, 50)
+
